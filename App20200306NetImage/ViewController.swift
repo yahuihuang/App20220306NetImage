adfe65908bc330ddde692c8d588d0a2da60fb488
Huang Grace <huang.yahui.grace@gmail.com>
2022-02-20 15:39:29 +0800

    multi thread implement

--- App20200306NetImage/ViewController.swift
+++ App20200306NetImage/ViewController.swift
@@ -14,14 +14,24 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         let imgPath = "https://warrant.tssco.com.tw/warrantweb/assets/img/mainPageNew/pick-warrants-h-img.png"
+
         let url = URL(string: imgPath)!
+        print("start \(Date().timeIntervalSince1970)")
+        
+        DispatchQueue.global().async {
+            do {
+                let imageData = try Data(contentsOf: url)
+                DispatchQueue.main.async {
+                    self.myImage.image = UIImage(data: imageData)
+                    print("show myImage \(Date().timeIntervalSince1970)")
+                }
                 
-        do {
-            let imageData = try Data(contentsOf: url)
-            self.myImage.image = UIImage(data: imageData)
-        } catch {
-            print(error.localizedDescription)
+            } catch {
+                print(error.localizedDescription)
+            }
         }
+
+        print("end \(Date().timeIntervalSince1970)")
     }
 
 
