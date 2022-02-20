356026f3bdc3433002cc8264c30923def998b13c
Huang Grace <huang.yahui.grace@gmail.com>
2022-02-20 15:28:55 +0800

    add image from internet

--- App20200306NetImage/ViewController.swift
+++ App20200306NetImage/ViewController.swift
@@ -9,9 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myImage: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let imgPath = "https://warrant.tssco.com.tw/warrantweb/assets/img/mainPageNew/pick-warrants-h-img.png"
+        let url = URL(string: imgPath)!
+                
+        do {
+            let imageData = try Data(contentsOf: url)
+            self.myImage.image = UIImage(data: imageData)
+        } catch {
+            print(error.localizedDescription)
+        }
     }
 
 
